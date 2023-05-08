37bf49180d345efa1b6857bc392a51d3c4012056
John <john.saehwan.lee@gmail.com>
2023-05-08 19:18:19 -0400

    saved skeleton of backend testing suite

--- server/__test__/backend.test.tsx
+++ server/__test__/backend.test.tsx
@@ -1,4 +1,46 @@
 // import React from 'React';
 // import "@testing-library/jest-dom";
+import { describe, it, afterEach, expect } from '@jest/globals';
+// import * as jest from '@jest/globals';
+import request from 'supertest';
+const server = 'http://localhost:3000';
 
-describe('Testing Suite for Routes', () => {});
+//request(server).post('/api/signup').send({ username: testUser, password: testPass, arn: testArn, region: testRegion }).expect(200)
+describe('Testing Suite for Routes', () => {
+  const testUser = 'jest';
+  const testPassword = 'jest';
+
+  describe('POST/signup', () => {
+    // afterEach(()=> request(server).delete('/api/user').send({username: testUser}));
+    const response = request(server)
+      .post('/signup')
+      .send({ username: testUser, password: testPassword });
+    it('Gives a status code of 200', () => expect(response.status).toBe(200));
+
+    it('Successfully creating a user in the database', () =>
+      expect(response.body.username).toEqual(testUser));
+  });
+
+  describe('POST/login', () => {
+    const response = request(server)
+      .post('/login')
+      .send({ username: testUser, password: testPassword });
+    it('Gives a status code of 200', () => expect(response.status).toBe(200));
+
+    it('Successfully matches ');
+  });
+
+  describe('GET/get flashcards', () => {});
+
+  describe('POST/make flashcards', () => {});
+
+  describe('GET/check answers', () => {});
+
+  describe('DELETE/delete a flashcard', () => {});
+
+  describe('PUT/update a flashcard', () => {});
+
+  describe('GET/user data/streak on login', () => {});
+
+  describe('PUT/update the user data/streak', () => {});
+});
